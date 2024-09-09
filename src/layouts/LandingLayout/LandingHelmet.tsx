719b46b93bda7e16de71456994f3fedc02d2f9d0
ktyudha <ktyudha@gmail.com>
2024-09-09 19:17:07 +0700

    ktyudha initi vercel

--- src/layouts/LandingLayout/LandingHelmet.tsx
+++ src/layouts/LandingLayout/LandingHelmet.tsx
@@ -5,14 +5,23 @@ const LandingHelmet: FunctionComponent = () => {
   return (
     <div className="application">
       <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Water+Brush&display=swap"
           rel="stylesheet"
+        /> */}
+
+        {/* DaisyUI */}
+        {/* <link
+          href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css"
+          rel="stylesheet"
+          type="text/css"
         />
+        <script src="https://cdn.tailwindcss.com"></script> */}
+
         {/* css */}
-        <link
+        {/* <link
           rel="stylesheet"
           href="/assets/vendors/bootstrap/css/bootstrap.min.css"
         />
@@ -56,20 +65,20 @@ const LandingHelmet: FunctionComponent = () => {
           rel="stylesheet"
           href="/assets/vendors/owl-carousel/assets/owl.theme.default.min.css"
         />
-        <link rel="stylesheet" href="/assets/css/eduact.css" />
+        <link rel="stylesheet" href="/assets/css/eduact.css" /> */}
 
         {/* js */}
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        {/* <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxchimp/1.3.0/jquery.ajaxchimp.min.js"></script>
-        {/* <script
+        <script
           type="text/javascript"
           src="/assets/vendors/jquery/jquery-3.5.1.min.js"
-        /> */}
+        />
         <script
           type="text/javascript"
           src="/assets/vendors/bootstrap/js/bootstrap.bundle.min.js"
         />
-        {/* <script type='text/javascript' src='/assets/vendors/bootstrap-select/bootstrap-select.min.js' /> */}
+        <script type='text/javascript' src='/assets/vendors/bootstrap-select/bootstrap-select.min.js' />
         <script
           type="text/javascript"
           src="/assets/vendors/jquery-ui/jquery-ui.js"
@@ -139,7 +148,7 @@ const LandingHelmet: FunctionComponent = () => {
           type="text/javascript"
           src="/assets/vendors/isotope/isotope.js"
         />
-        <script type="text/javascript" src="/assets/js/eduact.js" />
+        <script type="text/javascript" src="/assets/js/eduact.js" /> */}
       </Helmet>
     </div>
   );
